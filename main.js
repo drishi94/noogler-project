ca979a42a6e7f0e8851dc1e1112e439852c8b481
Devvret Rishi <devvret@devvret-macpro.roam.corp.google.com>
2016-08-17 13:57:39 -0700

    things work right now I think adding key functionality that will break everything

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 
 function Ask(){
-  this.checkSetup();
-
   this.questionList = document.getElementById('questions')
   this.questionForm = document.getElementById('question-form');
 	this.signOutButton = document.getElementById('sign-out');
@@ -14,6 +12,10 @@ function Ask(){
   this.signOutButton.addEventListener('click', this.signOut.bind(this));
   this.questionForm.addEventListener('submit', this.saveQuestion.bind(this));
 
+  this.answerButton = document.getElementById('answer-button');
+  this.answerButton = addEventListener('click', this.introBox.bind(this));
+
+  // this.answerButton = addEventListener('click', this.introBox.bind(this));
     // Toggle for the button.
   // var buttonTogglingHandler = this.toggleButton.bind(this);
   // this.messageInput.addEventListener('keyup', buttonTogglingHandler);
@@ -60,13 +62,13 @@ Ask.prototype.loadQuestions = function() {
   this.questionsRef = this.database.ref('questions');
   //remove all other listeners
   this.questionsRef.off();
-
   var setMessage = function(data){
     var val = data.val();
     this.displayQuestion(data.key, val.name, val.text);
   }.bind(this);
-  this.questionsRef.limitToLast(12).on('child_added', setMessage);
-  this.questionsRef.limitToLast(12).on('child_changed', setMessage);
+  console.log(this.questionsRef.val);
+  this.questionsRef.limitToLast(20).on('child_added', setMessage);
+  this.questionsRef.limitToLast(20).on('child_changed', setMessage);
   };
 
 
@@ -78,8 +80,9 @@ Ask.prototype.displayQuestion = function(key, name, text){
     '<div class="question-container" align="left" style="margin-bottom: 100px;">' +
       '<h2 class="question" style="margin-left:50px"></h2>' +
       '<div class="name" style= "margin-left:50px"></div>' +
-      '<button style="border:none; background-color: white; text-align: center; font-size: 16px; color: blue; padding: 10px 10px; margin-left:45px;"> Comment </button>' +
+      '<button id="answer-button' + key + '" style="border:none; background-color: white; text-align: center; font-size: 16px; color: blue; padding: 10px 10px; margin-left:45px;"> Answer </button>' +
       '<button style="border:none; background-color: white; text-align: center; font-size: 16px; color: green; padding: 10px 10px; margin-left:45px;"> Upvote </button>' +
+      '<div id = "answer-box-list"> </div>' +
 
     '</div>';
     div = container.firstChild;
@@ -98,35 +101,27 @@ Ask.prototype.displayQuestion = function(key, name, text){
 };
 
 
-
-
 Ask.prototype.saveQuestion = function(){
   //NEED TO ADD THE ACTUAL QUESTION 
   var currentUser = this.auth.currentUser;
   this.questionsRef.push({name: currentUser.displayName, text:this.questionInput.value})
 };
 
-Ask.prototype.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions.');
-  } else if (config.storageBucket === '') {
-    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
-        'actually a Firebase bug that occurs rarely. ' +
-        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
-        'and make sure the storageBucket attribute is not empty. ' +
-        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
-        'displayed there.');
-  }
-};
+Ask.prototype.introBox = function(){
+  this.answerBoxList = document.getElementById('answer-box-list');
+  var answerBox = document.createElement("div");
+  answerBox.innerHTML = '<div>' + '<form action="#">' + 'Comment: <input type="text">' + '</form>' + '</div>';
+  this.answerBoxList.appendChild(answerBox);
+  // console.log(this.answerBox);
+
+}
 
 
-
-// Ask.prototype.signOut = function() {
-//   	window.location.href = "index.html";
-
-  // this.auth.signOut();
+// Ask.prototype.saveAnswer = function(){
+//   var currentUser = this.auth.currentUser;
+//   this.answersRef.push({name: currentUser.displayName, text: this.answerInput.value});
 // }
+
 
 window.onload = function() {
   window.Ask = new Ask();
